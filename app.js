087a9fb928442555aaaa876dc15d90148aed7594
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-02-10 19:51:22 +0530

    css added as middleware

--- app.js
+++ app.js
@@ -8,20 +8,21 @@ const port = process.env.PORT || 3000; // setting up port on PORT or 3000
 app.set('view engine', 'ejs'); // set the view engine to ejs
 
 app.use(bodyParser.urlencoded({extended:true})); //parses incoming request bodies in a middleware before you handle it
+app.use(express.static("public"));
 
-var items = ["First item", "Second item"]; // init array for all to do li elements 
+let items = ["First item", "Second item"]; // init array for all to do li elements 
 
 app.get("/", (req, res) => {
 
-    var today = new Date(); // get current date
+    let today = new Date(); // get current date
 
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options); // returns a Date object as a string
+    let day = today.toLocaleDateString("en-US", options); // returns a Date object as a string
 
     res.render("list", {kindOfDay: day, newListItems: items }); // use res.render to load up an ejs view file
     // it renders list.ejs file and passes the kindOfDay and newListItems variable values
@@ -29,7 +30,7 @@ app.get("/", (req, res) => {
 
 app.post ("/", (req, res) =>{
 
-    var item = req.body.newItem; // accepts 'newItem' data from the client side input form and stores in item var
+    let item = req.body.newItem; // accepts 'newItem' data from the client side input form and stores in item var
 
     items.push(item); // pushes new item to the items array
 
@@ -39,5 +40,3 @@ app.post ("/", (req, res) =>{
 app.listen(port, ()=>{
     console.log(`Server listening to ${port}`); // server started at port
 })
-
-// basic prototype is ready
