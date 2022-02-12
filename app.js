de015725ed1bb283569c761b2084351c9bf3631d
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-02-12 20:20:56 +0530

    priting the name value from variable

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const { stringify } = require('nodemon/lib/utils');
 
 const app = express();
 
@@ -10,11 +12,36 @@ app.set('view engine', 'ejs'); // set the view engine to ejs
 app.use(bodyParser.urlencoded({extended:true})); //parses incoming request bodies in a middleware before you handle it
 app.use(express.static("public")); // use public folder to access static files like css
 
-let items = ["First item", "Second item"]; // init array for all to do li elements 
+// let items = ["First item", "Second item"]; init array for all to do li elements 
+
 let workItems = []; //init array for work items list
 
 
+mongoose.connect("mongodb+srv://LearnDB:XMHluSqZjKSCOoaT@cluster0.fsj5n.mongodb.net/toDoList?retryWrites=true&w=majority"); //connecting to the database on mongoDB Atlas
 
+const itemsSchema = { // creating a schema for DB model
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema); // creating a model using previous schema, usually models are n
+
+const item1 = new Item({ // creating a new item from the Item Model
+    name: "Welcome to ToDoList"
+}); 
+
+const item2 = new Item({
+    name: "Welcome to ToDoList"
+}); 
+
+const item3 = new Item({
+    name: "Welcome to ToDoList"
+}); 
+
+const defaultItems = [item1, item2, item3] // creating an array with previously created items
+
+Item.insertMany(defaultItems, (err)=>{ // insert newly created array to Item
+    console.log('error in inserting')
+});
 
 app.get("/", (req, res) => {
 
@@ -28,8 +55,13 @@ app.get("/", (req, res) => {
 
     let day = today.toLocaleDateString("en-US", options); // returns a Date object as a string
 
-    res.render("list", {listTitle: day, newListItems: items }); // use res.render to load up an ejs view file
+    Item.find({}, (err, foundItems)=>{ // {} finds everything, foundItems contains everything
+        
+        res.render("list", {listTitle: day, newListItems: foundItems }); // use res.render to load up an ejs view file
     // it renders list.ejs file and passes the kindOfDay and newListItems variable values
+    
+    })
+
 });
 
 
